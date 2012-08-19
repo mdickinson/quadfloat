ef3e7ea4931e60ea03a87490b218029b6d6fac00
Mark Dickinson <mdickinson@enthought.com>
2012-08-19 18:06:14 +0100

    Add proper unittest support to test_docstrings.

--- quadfloat/tests/test_docstrings.py
+++ quadfloat/tests/test_docstrings.py
@@ -1,9 +1,19 @@
 import doctest
+import unittest
 
 import quadfloat.arithmetic
 import quadfloat.binary_interchange_format
 
 
+def load_tests(loader, tests, ignore):
+    modules = [
+        quadfloat.arithmetic,
+        quadfloat.binary_interchange_format,
+    ]
+    for module in modules:
+        tests.addTests(doctest.DocTestSuite(module=module))
+    return tests
+
+
 if __name__ == '__main__':
-    doctest.testmod(quadfloat.arithmetic)
-    doctest.testmod(quadfloat.binary_interchange_format)
+    unittest.main()
