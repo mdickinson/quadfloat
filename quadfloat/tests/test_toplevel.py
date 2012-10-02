352feafb355c7d05092b2e7a1e57c6022fd10964
Mark Dickinson <mdickinson@enthought.com>
2012-10-02 19:04:13 +0100

    Add missing import; clean up whitespace.

--- quadfloat/tests/test_toplevel.py
+++ quadfloat/tests/test_toplevel.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class TestToplevel(unittest.TestCase):
     def test_import_predefined_formats(self):
         from quadfloat import float16
@@ -7,4 +10,3 @@ class TestToplevel(unittest.TestCase):
 
     def test_import_binary_interchange_format(self):
         from quadfloat import BinaryInterchangeFormat
-        
