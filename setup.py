bfd1fc0dab3ec645bb2633c2159e6f864813e78e
Mark Dickinson <mdickinson@enthought.com>
2012-10-02 18:47:22 +0100

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='quadfloat',
+    version='0.1',
+    description='IEEE 754 binary floating-point',
+    author='Mark Dickinson',
+    packages=['quadfloat', 'quadfloat.tests'],
+)
