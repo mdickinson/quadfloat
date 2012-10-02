16eabb3304eac3b5ad0e2373e1bf0fa6fb36cdfa
Mark Dickinson <mdickinson@enthought.com>
2012-10-02 19:05:46 +0100

    Fix imported names.

--- quadfloat/tests/test_toplevel.py
+++ quadfloat/tests/test_toplevel.py
@@ -3,10 +3,10 @@ import unittest
 
 class TestToplevel(unittest.TestCase):
     def test_import_predefined_formats(self):
-        from quadfloat import float16
-        from quadfloat import float32
-        from quadfloat import float64
-        from quadfloat import float128
+        from quadfloat import binary16
+        from quadfloat import binary32
+        from quadfloat import binary64
+        from quadfloat import binary128
 
     def test_import_binary_interchange_format(self):
         from quadfloat import BinaryInterchangeFormat
