428647bd48779c7bc9b49a141ff7bcd68e9caa23
Mark Dickinson <mdickinson@enthought.com>
2013-06-09 12:10:21 +0100

    Move test data into separate files; add test runner that runs all these tests.

--- quadfloat/tests/test_docstrings.py
+++ quadfloat/tests/test_docstrings.py
@@ -1,14 +1,18 @@
 import doctest
+import unittest
 
 import quadfloat.arithmetic
 import quadfloat.binary_interchange_format
 
 
-def load_tests(loader, tests, ignore):
-    modules = [
-        quadfloat.arithmetic,
-        quadfloat.binary_interchange_format,
-    ]
-    for module in modules:
-        tests.addTests(doctest.DocTestSuite(module=module))
-    return tests
+doctest_modules = [
+    quadfloat.arithmetic,
+    quadfloat.binary_interchange_format,
+]
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    for module in doctest_modules:
+        suite.addTests(doctest.DocTestSuite(module=module))
+    return suite
